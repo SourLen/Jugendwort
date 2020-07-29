77d7bc022fcb66cf966cb706792d61a24de66f24
kadeeq <62156037+kadeeq@users.noreply.github.com>
2020-07-29 17:01:08 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 import random
 import time
-
+from selenium.common.exceptions import NoSuchElementException
 
 class Bot:
     def __init__(self):
 
         wort1 = input("Wort, das angegeben weden soll:")
         wort = wort1
-        ChromeDriverPfad = input("Pfad ihrer Chromedriver.exe Datei:")
-        self.driver = webdriver.Chrome(ChromeDriverPfad)
+        chromedriverpfad = input("Pfad ihrer Chromedriver.exe Datei:")
+        self.driver = webdriver.Chrome(chromedriverpfad)
         self.driver.get("https://www.surveymonkey.com/r/7JZRVLJ?embedded=1")
         time.sleep(2)
         main = True
@@ -18,11 +18,13 @@ class Bot:
                    "/html/body/main/article/section/form/div[1]/div[1]/div/div/fieldset/div/select/option[3]",
                    "/html/body/main/article/section/form/div[1]/div[1]/div/div/fieldset/div/select/option[4]",
                    "/html/body/main/article/section/form/div[1]/div[1]/div/div/fieldset/div/select/option[5]"]
+
         while main:
             i += 1
             ran = random.randint(0, 3)
             self.driver.find_element_by_id("463803414").click()
-            time.sleep(1)
+            time.sleep(1.25
+                       )
             self.driver.find_element_by_xpath(options[ran]).click()
             if i == 1:
                 word = self.driver.find_element_by_xpath('//*[@id="463803684"]').send_keys(wort)
@@ -31,9 +33,14 @@ class Bot:
                 self.driver.find_element_by_xpath(
                     "//*[@id='question-field-483089934']/fieldset/div/div/div/div/label/span[1]").click()
 
-            self.driver.find_element_by_xpath("//*[@id='patas']/main/article/section/form/div[2]/button").click()
-            self.driver.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button[1]').click()
-            print(i)
+            try:
+                self.driver.find_element_by_xpath("//*[@id='patas']/main/article/section/form/div[2]/button").click()
+            except NoSuchElementException:
+                time.sleep(1)
+            try:
+                self.driver.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button[1]').click()
+            except NoSuchElementException:
+                time.sleep(1)
 
 
 larry = Bot()
